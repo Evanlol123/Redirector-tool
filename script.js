3a12655baee5d67ed934ca9698341c0d8c41776a
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-09-22 10:49:59 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-  (function(){var script=document.createElement("script");script.src="https://cdn.jsdelivr.net/npm/eruda";document.body.append(script);script.onload=function(){eruda.init();console.log("Script Loaded")}})();
+//eruda because useful for debugging 
+(function(){var script=document.createElement("script");script.src="https://cdn.jsdelivr.net/npm/eruda";document.body.append(script);script.onload=function(){eruda.init();console.log("Script Loaded")}})();
 const form = document.querySelector('form');
 const input = document.querySelector('#url-input');
+const tlds = [".com", ".org", ".net", ".info", ".biz", ".us", ".uk", ".co", ".me", ".io", ".ai", ".app", ".dev", ".xyz", ".store", ".online", ".site", ".tech", ".design", ".live", ".tv", ".cc", ".cd", ".name", ".pro", ".jobs", ".mobi", ".tel", ".ws", ".cloud", ".shop", ".bank", ".blog", ".photo", ".restaurant", ".travel", ".fun", ".news", ".game", ".art", ".space", ".agency", ".health", ".education", ".finance", ".consulting", ".video", ".music", ".auto", ".guru", ".chat", ".press", ".digital", ".reviews", ".solutions", ".homes", ".family", ".community", ".tips", ".voting", ".science", ".engineering", ".insurance", ".construction", ".fishing", ".photography", ".farm", ".recipes", ".wedding", ".events", ".lifestyle", ".fashion", ".games"]
+let hasTLD = tlds.some(tld => url.endsWith(tld));
 
 form.addEventListener('submit', function(event) {
 	event.preventDefault();
 	let url = input.value;
-	if (!url.startsWith('http://') && !url.startsWith('https://')) {
+	if (!url.startsWith('http://') && !url.startsWith('https://') && hasTLD) {
 		url = 'https://' + url;
 	}
-	window.location.href = url;
+	if (!url.startsWith('http://') && !url.startsWith('https://') && !hasTLD) {
+		url = 'https://www.google.com/search?q=' + url;
+	}
+
 });
